66a9b14ce0701545ae16bfb31b73e15fe6e7fa91
Jakub Sukiennik <kuba1207710@gmail.com>
2020-10-09 21:59:12 +0200

    moving to style folder

--- client/src/Pages/Following.tsx
+++ client/src/Pages/Following.tsx
@@ -3,27 +3,10 @@ import { useParams } from "react-router-dom";
 import Loader from "../Components/Loader";
 import { useRootStore } from "../Store/RootStore";
 import useFollowing from "../utils/hooks/useFollowing";
-import styled from "styled-components";
+
 import Default from "../Images/default_profile_400x400.png";
-const UserWrapper = styled.div`
-  padding: 0.4rem;
-  color: white;
-  display: flex;
-`;
-const UserTitle = styled.h4`
-  padding: 0.2rem;
-`;
-const Avatar = styled.img`
-  background-color: ${(props) => props.theme.colors.primary};
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-`;
-const InfoWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 0.3rem;
-`;
+import { Avatar, InfoWrapper, UserTitle, UserWrapper } from "../Style/ComponentStyles/FollowingAndFollowersStyles";
+
 
 const Following = () => {
   const { userStore } = useRootStore();
